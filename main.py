d05e101f2432e8dd6b71b27ec451522fc7606744
TerBearBaby <111261249+TerBearBaby@users.noreply.github.com>
2022-09-01 15:23:58 -0600

    Update main.py
    Added an echo Command
    And fixed bot info

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import discord, json, os, random, asyncio, string, time, hostingserver, requests
+from Cog import staff
 from discord.ext import commands, tasks
 from datetime import datetime
 from hostingserver import keep_alive
-
+from discord.ui import Button, View
 from requests import get
 
 # imports things needed for bot (modules needed)
-
-# bot token to activate bot
 
 member = discord.Member
 
@@ -30,7 +29,6 @@ intents.message_content = True
 
 client = commands.Bot(command_prefix=['m?', 'M?'], intents=intents, help_command=None, case_insensitive=True) 
 
-
 @client.event
 async def on_ready():
   print(f"logged into {client.user.name}")
@@ -49,104 +47,12 @@ async def before_update_presence():
 
 update_presence.start()
 
-@client.command()
+
+@client.slash_command(name="help", description="Help page with commands for Muzocco!")
 async def help(ctx):
-  embed=discord.Embed(title="Hey There!\nMuzocco! doesnt have too many commands yet!", description="Commands that are available for beta update are listed below (bigger desc coming soon!)", color=0x2F3136)
-  embed.add_field(name="M?help", value="Shows help page for Muzocco!", inline=True)
-  embed.add_field(name="M?bi/M?botinfo", value="Shows bot info for Muzocco!", inline=True)
-  embed.add_field(name="M?ui/userinfo", value="Shows user info for pinged user", inline=True)
-  embed.add_field(name="M?si/serverinfo", value="Shows info for a server Muzocco! is in", inline=True)
-  embed.add_field(name="M?meme", value="Run this command to see an epic meme", inline=True)
-  embed.add_field(name="M?av/avatar", value="Shows avatar (pfp) for pinged user", inline=True)
-  embed.add_field(name="M?links", value="Links for invitation to support and to invite Muzocco!")
-  embed.set_footer(text=f"Copyright © Muzocco! 2022 All Rights Reserved")
-  embed.timestamp = discord.utils.utcnow()
-  await ctx.reply(embed=embed)
-
-@client.command(aliases=["bi"])
-async def botinfo(ctx):
-  embed = discord.Embed(color=0x2F3136)
-  embed.add_field(name="Host:", value="```\nreplit.com\n```", inline=False)
-  embed.add_field(name="Ping:", value=f"```\n{round (client.latency * 1000)} ms\n```", inline=False)
-  embed.add_field(name="Uptime:", value=f"```\n{time.time() - starttime}\n```", inline=False)
-  embed.add_field(name="Date Created:", value="```\nAugust 8th, 2022\n```", inline=False)
-  await ctx.reply(embed=embed)
-
-
-@client.command(aliases=["ui"])
-async def userinfo(ctx, user: discord.Member = None):
-  guild = ctx.guild
-  if user == None:
-      user = ctx.author
-
-  rlist = []
-  for role in user.roles:
-      if role.name != "@everyone":
-          rlist.append(role.mention)
-      else:
-          rlist.append("No roles")
-
-  b = " | ".join(rlist)
-  embed = discord.Embed(title=f"{user}", color=0x2F3136)
-  embed.add_field(name=f"💬Users Display Name: {user.display_name}", value="Displays A Users Nickname If They Have One", inline=False)
-  embed.add_field(name=f"🆔User ID: {user.id}", value="User ID", inline=False)
-  embed.add_field(name=f"🔊Playing Status Or Custom Status: {user.activity}", value="Displaying What User Is Playing", inline=False)
-  embed.add_field(name=f"🚦User Status: {user.status}", value="Displaying User Status", inline=False)
-  embed.add_field(name=f"👥User Roles:", value=''.join([b]), inline=False)
-  embed.add_field(name=f"📆Joined Discord On: {user.created_at}", value="Displaying When User Joined Discord", inline=False)
-  await ctx.reply(embed=embed)
-
-
-@client.command(aliases=["av"])
-async def avatar(ctx, member: discord.Member = None):
-  if not ctx.author.bot:
-      if member == None:
-          member = ctx.author
-  embed = discord.Embed(title=f"", color=0x2F3136)
-  embed.set_author(name=f"{member}'s Avatar", icon_url=f"{member.avatar.url}")
-  embed.set_image(url=f"{member.avatar.url}")
-  await ctx.reply(embed=embed)
-
-
-@client.command()
-async def meme(ctx):
-    content = get("https://meme-api.herokuapp.com/gimme").text
-    data = json.loads(content)
-
-    title = data["title"]
-    link = data["postLink"]
-    img = data["url"]
-    likes = data["ups"]
-
-    meme = discord.Embed(description=f"[{title}]({link})", color=0x2F3136).set_image(url=img)
-    meme.set_footer(text=f"{likes}👍")
-    await ctx.reply(embed=meme)
-
-
-@client.command(aliases=["si"])
-async def serverinfo(ctx, member: discord.Member = None):
-  if not ctx.author.bot:
-    if member == None:
-      member = ctx.author
-  embed = discord.Embed(title=f"{ctx.guild.name} Info", description="Information of this Server", color=0x2F3136)
-  embed.add_field(name='🆔Server ID', value=f"{ctx.guild.id}", inline=True)
-  embed.add_field(name='📆Created On', value=ctx.guild.created_at.strftime("%b %d %Y"), inline=True)
-  embed.add_field(name='👑Owner', value=f"{ctx.guild.owner.mention}", inline=True)
-  embed.add_field(name='👥Members', value=f'{ctx.guild.member_count} Members', inline=True)
-  embed.add_field(name='💬Channels', value=f'{len(ctx.guild.text_channels)} Text | {len(ctx.guild.voice_channels)} Voice', inline=True)
-  await ctx.reply(embed=embed)
-
-
-@client.command()
-async def links(ctx):
-  supportem=discord.Embed(title="Muzocco! Support Server", url="https://dsc.gg/muzocco-support", description="Hey there, Muzocco! Support Server has all of Musocco's updates & GitHub's and more!\n\nJoin now!\nhttps://discord.gg/s9NtJADv\nhttps://dsc.gg/muzocco-support", color=0x2F3136)
-  inviteem=discord.Embed(title="Muzocco!", url="https://dsc.gg/muzocco", description="This is Muzocco! The coolest bot ever, invite this bot to your server to listen to music in a VC or use my fun commands!\n\nhttps://dsc.gg/muzocco", color=0x2F3136)
-  await ctx.reply(embed=inviteem)
-  await ctx.reply(embed=supportem)
-
-
-@client.slash_command(name="help")
-async def help(ctx):
+  link = Button(label='Dashboard', url='https://terbearbaby.github.io/Muzocco-')
+  view = View()
+  view.add_item(link)
   embed=discord.Embed(title="Hey There!\nMuzocco! doesnt have too many commands yet!", description="Commands that are available for beta update are listed below (bigger desc coming soon!)", color=0x2F3136)
   embed.add_field(name="/help", value="Shows help page for Muzocco!", inline=True)
   embed.add_field(name="/bi or /botinfo", value="Shows bot info for Muzocco!", inline=True)
@@ -154,23 +60,34 @@ async def help(ctx):
   embed.add_field(name="/si or /serverinfo", value="Shows info for a server Muzocco! is in", inline=True)
   embed.add_field(name="/meme", value="Run this command to see an epic meme", inline=True)
   embed.add_field(name="/av or /avatar", value="Shows avatar (pfp) for pinged user", inline=True)
-  embed.add_field(name="/links", value="Links for invitation to support and to invite Muzocco!")
+  embed.add_field(name="/mute", value="Mutes specified user")
+  embed.add_field(name="/unmute", value="Unmutes specified user")
+  embed.add_field(name="/kick", value="Kicks specified user")
+  embed.add_field(name="/ban", value="Bans specified user")
+  embed.add_field(name="/unban", value="Unbans specified user")
+  embed.add_field(name="/echo", value="Repeats whatever you say")
   embed.set_footer(text=f"Copyright © Muzocco! 2022 All Rights Reserved")
   embed.timestamp = discord.utils.utcnow()
+  await ctx.respond(embed=embed, view=view)
+
+
+@client.slash_command(name="echo", description="Repeats whatever you say [ /echo (content) ]")
+async def echo(ctx, *, content):
+  embed=discord.Embed(title=f"{content}", description=f"Repeated {content}", color=0x2F3136)
   await ctx.respond(embed=embed)
 
 
-@client.slash_command(name="botinfo",  aliases=["bi"])
+@client.slash_command(name="botinfo", description="Bot info for Muzocco!")
 async def botinfo(ctx):
-  embed = discord.Embed(color=0x2F3136)
-  embed.add_field(name="Host:", value="```\nreplit.com\n```", inline=False)
-  embed.add_field(name="Ping:", value=f"```\n{round (client.latency * 1000)} ms\n```", inline=False)
-  embed.add_field(name="Uptime:", value=f"```\n{time.time() - starttime}\n```", inline=False)
-  embed.add_field(name="Date Created:", value="```\nAugust 8th, 2022\n```", inline=False)
-  await ctx.respond(embed=embed)
+  link = Button(label='Dashboard', url='https://terbearbaby.github.io/Muzocco-')
+  view = View()
+  view.add_item(link)
+  embed = discord.Embed(title="Bot Information", description=f"**Head Info:**\n**Host:** https://Muzocco-Test.pianoidol.repl.co\n**Date Created:** August 8th, 2022 8/8/2022\n\n**Statistics:**\nPing:** {round (client.latency * 1000)} ms\n**Uptime:** {time.time() - starttime} seconds\n\n**Other Info:**\n**Partners:** None", color=0x2F3136)
+  embed.add_footer(text="DM Terbearbaby#6960 if you have any complaints")
+  await ctx.channel.respond(embed=embed, view=view)
 
 
-@client.slash_command(name="userinfo", aliases=["ui"])
+@client.slash_command(name="userinfo", description="User info for specified user")
 async def userinfo(ctx, user: discord.Member = None):
   guild = ctx.guild
   if user == None:
@@ -194,7 +111,7 @@ async def userinfo(ctx, user: discord.Member = None):
   await ctx.respond(embed=embed)
 
 
-@client.slash_command(name="avatar", aliases=["av", "pfp"])
+@client.slash_command(name="avatar", description="Shows profile picture for specified user")
 async def avatar(ctx, member: discord.Member = None):
   if not ctx.author.bot:
       if member == None:
@@ -205,7 +122,7 @@ async def avatar(ctx, member: discord.Member = None):
   await ctx.respond(embed=embed)
 
 
-@client.slash_command(name="meme")
+@client.slash_command(name="meme", description="Funny memes")
 async def meme(ctx):
     content = get("https://meme-api.herokuapp.com/gimme").text
     data = json.loads(content)
@@ -220,7 +137,7 @@ async def meme(ctx):
     await ctx.respond(embed=meme)
 
 
-@client.slash_command(name="serverinfo", aliases=["si"])
+@client.slash_command(name="serverinfo", description="Info for a server that command is ran in")
 async def serverinfo(ctx, member: discord.Member = None):
   if not ctx.author.bot:
     if member == None:
@@ -234,12 +151,23 @@ async def serverinfo(ctx, member: discord.Member = None):
   await ctx.respond(embed=embed)
 
   
-@client.slash_command()
+@client.slash_command(name="links", description="Links for Muzocco!")
 async def links(ctx):
+  link = Button(label='Dashboard', url='https://terbearbaby.github.io/Muzocco-')
+  view = View()
+  view.add_item(link)
   supportem=discord.Embed(title="Muzocco! Support Server", url="https://dsc.gg/muzocco-support", description="Hey there, Muzocco! Support Server has all of Musocco's updates & GitHub's and more!\n\nJoin now!\nhttps://discord.gg/s9NtJADv\nhttps://dsc.gg/muzocco-support", color=0x2F3136)
   inviteem=discord.Embed(title="Muzocco!", url="https://dsc.gg/muzocco", description="This is Muzocco! The coolest bot ever, invite this bot to your server to listen to music in a VC or use my fun commands!\n\nhttps://dsc.gg/muzocco", color=0x2F3136)
   await ctx.respond(embed=inviteem)
-  await ctx.respond(embed=supportem)
+  await ctx.respond(embed=supportem, view=view)
+
+
+
+
+for file in os.listdir('Cog'): 
+    if file.endswith('.py'):
+      client.load_extension(f'Cog.' + file[:-3]) 
+
 
 keep_alive()
 starttime = time.time()
